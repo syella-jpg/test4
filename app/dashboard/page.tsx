410065e8e45110c5bd4a4064850e1c42b0afcdcc
madikabp <231712547@students.uajy.ac.id>
2025-05-09 18:15:03 +0700

    test

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,19 +1,3 @@
-
-import { lusitana } from '../ui/fonts';
-
 export default async function Page(){
-
-  return (
-    <main>
-      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
-        Dashboard
-      </h1>
-      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-   
-      </div>
-      <div className="mt-6 grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-   
-      </div>
-    </main>
-  );
+  return <p> Dashboard Page </p>
 }
